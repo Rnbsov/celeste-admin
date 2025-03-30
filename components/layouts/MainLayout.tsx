9cab3cdc2eea4f3cad3329d8f569e326b9efffe6
Rnbsov <rnbsov@gmail.com>
2025-03-30 08:09:24 +0500

    ✨ ui: Remove unnecessary `padding`
    Changelog: ui

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -11,7 +11,7 @@ export function MainLayout({ children }: { children: ReactNode }) {
       <div className='flex'>
         <Sidebar />
 
-        <div className='relative flex flex-1 flex-col lg:ml-72.5'>
+        <div className='relative flex flex-1 flex-col'>
           {/* <Header /> */}
 
           <main className='mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 flex-1'>{children}</main>
