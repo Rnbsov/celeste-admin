bbde40681b9b8a05c4e1491d88c37754e907d11e
Rnbsov <rnbsov@gmail.com>
2025-03-30 08:09:04 +0500

    ✨ ui: Update sidebar item colors
    Changelog: ui

--- components/Sidebar/SidebarItem.tsx
+++ components/Sidebar/SidebarItem.tsx
@@ -23,8 +23,8 @@ const SidebarItem = ({ item }: { item: MenuItem }) => {
           href={item.route}
           className={cn(
             'group relative flex justify-between items-center rounded-lg px-4 py-2 font-medium text-[#88908d] duration-300 ease-in-out cursor-pointer hover:text-[#111313]',
-            isActive && 'bg-emerald-400 text-[#111313]',
-            !isActive && 'hover:bg-emerald-400/80'
+            isActive && 'bg-[#b8ce95] text-[#111313]',
+            !isActive && 'hover:bg-[#b8ce95]/80'
           )}
         >
           <div className='flex gap-2.5'>
