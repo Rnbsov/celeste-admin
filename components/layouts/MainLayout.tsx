5bbcb49fee9a817f37e89e861fd942b41739a444
Rnbsov <rnbsov@gmail.com>
2025-03-30 07:49:25 +0500

    ✨ ui: Create `MainLayout` component
    Changelog: ui

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { ReactNode } from 'react'
+
+// import Header from '@/components/Header'
+import { Sidebar } from '@/components/Sidebar'
+
+export function MainLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <div className='flex'>
+        <Sidebar />
+
+        <div className='relative flex flex-1 flex-col lg:ml-72.5'>
+          {/* <Header /> */}
+
+          <main className='mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 flex-1'>{children}</main>
+        </div>
+      </div>
+    </>
+  )
+}
