8878ad8349075be8b92a2b9b73411b8631ffedfe
Rnbsov <rnbsov@gmail.com>
2025-03-30 07:44:29 +0500

    ✨ ui: Enable only dark colors
    Changelog: ui

---
+++ components/Sidebar/SidebarItem.tsx
@@ -0,0 +1,42 @@
+import { ChevronRight, LucideIcon } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+import { cn } from '@/lib/utils'
+
+export interface MenuItem {
+  icon: LucideIcon
+  label: string
+  route: string
+}
+
+const SidebarItem = ({ item }: { item: MenuItem }) => {
+  const pathname = usePathname()
+
+  const isActive = pathname === item.route
+
+  return (
+    <>
+      <li>
+        <Link
+          href={item.route}
+          className={cn(
+            'group relative flex justify-between items-center rounded-lg px-4 py-2 font-medium text-grayPrimary duration-300 ease-in-out cursor-pointer hover:text-darkPrimary',
+            isActive && 'bg-secondaryEmerald text-darkPrimary',
+            !isActive && 'hover:bg-secondaryEmerald/80'
+          )}
+        >
+          <div className='flex gap-2.5'>
+            <item.icon size={22} />
+            {item.label}
+          </div>
+
+          {isActive && <ChevronRight />}
+        </Link>
+      </li>
+    </>
+  )
+}
+
+export { SidebarItem }
