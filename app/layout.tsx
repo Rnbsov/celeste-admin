a1731d7bb4b3dc8bb9500262c6b40b6b09c6c1f3
Rnbsov <rnbsov@gmail.com>
2025-03-30 08:28:24 +0500

    ✨ ui: Update background color for `body`
    Changelog: ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0d0d0c]`}>
         <MainLayout>{children}</MainLayout>
       </body>
     </html>
